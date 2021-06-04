77b8efe7efec1e7d07f38111a8a5b04032294a16
Redfill Production <xmagher@gmail.com>
2021-06-04 15:51:48 -0400

    started joint creation

--- XM_AutoRig.py
+++ XM_AutoRig.py
@@ -14,6 +14,18 @@ class locatorRig(object):
             self.locator.setParent(self.parent)
 
 
+class jointRig(object):
+    def __init__(self, name, parent, location):
+        self.name = name
+        self.parent = parent
+        self.location = location
+        if self.parent != None:
+            self.joint = pm.joint(n=self.name,p=self.location)
+            self.joint.setParent(self.parent)
+            pm.joint(self.joint, e=True, sao="yup",zso=True, oj="xyz")
+        else:
+            self.joint = pm.joint(n=self.name,p=self.location)
+            pm.joint(self.joint, e=True, sao="yup", zso=True, oj="xyz")
 
 def CreateSetup(rigName):
     pm.frameLayout(XMAutorigWindow.settingFrame, e=True, en=True)
@@ -29,7 +41,9 @@ def CreateSetup(rigName):
 
     leg = locatorRig("leg", thigh.locator, (12,49,0))
 
-    foot = locatorRig("foot", leg.locator, (12,7,-3))
+    foot = locatorRig("foot", thigh.locator, (12,7,-4))
+
+    toe = locatorRig("toe", foot.locator, (12,1.5,11))
 
     shoulder = locatorRig("shoulder", neck.locator, (8,152,0))
 
@@ -56,26 +70,62 @@ def CreateSetup(rigName):
     XMAutorigWindow.rigLocator = rig_dict
     print(XMAutorigWindow.rigLocator)
 
+def Createjoint():
+    locator = XMAutorigWindow.rigLocator
+    pm.select(clear=True)
+
+    hips = jointRig("hips_bjnt", None, locator["hips"].getTranslation("world"))
+    spine = jointRig("spine_bjnt", hips.joint, (locator["hips"].getTranslation("world") + locator["neck"].getTranslation("world"))/2)
+    neck = jointRig("neck_bjnt", spine.joint, locator["neck"].getTranslation("world"))
+    head = jointRig("head_bjnt", neck.joint, locator["head"].getTranslation("world"))
+
+    pm.select(clear=True)
+    thigh = jointRig("l_upleg_bjnt", hips.joint, locator["thigh"].getTranslation("world"))
+    leg = jointRig("l_leg_bjnt", thigh.joint, locator["leg"].getTranslation("world"))
+    foot = jointRig("l_foot_bjnt", leg.joint, locator["foot"].getTranslation("world"))
+    pm.joint(thigh.joint, e=True, oj="xyz", ch=True)
+
+    pm.select(clear=True)
+    shoulder = jointRig("l_shoulder_bjnt", None, locator["shoulder"].getTranslation("world"))
+    arm = jointRig("l_arm_bjnt", shoulder.joint, locator["arm"].getTranslation("world"))
+    forearm = jointRig("l_forearm_bjnt", arm.joint, locator["forearm"].getTranslation("world"))
+    hand = jointRig("l_hand_bjnt", forearm.joint, locator["hand"].getTranslation("world"))
+    pm.joint(shoulder.joint, e=True, oj="xyz", ch=True )
+
+
+
 def DeleteSetup():
     pm.frameLayout(XMAutorigWindow.settingFrame, e=True, en=False)
     pm.delete(XMAutorigWindow.rigLocator["hips"])
 
 
-def FixElbow():
-    armPos = XMAutorigWindow.rigLocator["arm"].getTranslation("world")
-    elbowPos = XMAutorigWindow.rigLocator["forearm"].getTranslation("world")
-    handPos = XMAutorigWindow.rigLocator["hand"].getTranslation("world")
-    a = (armPos[1]-handPos[1])/(armPos[0]-handPos[0])
-    b = armPos[1]-(armPos[0]*a)
-    c = (elbowPos[0]*a)+b
-    XMAutorigWindow.rigLocator["forearm"].setTranslation((elbowPos[0],c,elbowPos[2]), "world")
+def FixElbow(x, p1, p2, p3):
+    armPos = p1.getTranslation("world")
+    elbowPos = p2.getTranslation("world")
+    handPos = p3.getTranslation("world")
+    if(x == "x"):
+        if(armPos[1] == handPos[1]):
+            p2.setTranslation((elbowPos[0], handPos[2], elbowPos[2]), "world")
+            pass
+        a = (armPos[1]-handPos[1])/(armPos[0]-handPos[0])
+        b = armPos[1]-(armPos[0]*a)
+        c = (elbowPos[0]*a)+b
+        p2.setTranslation((elbowPos[0], c, elbowPos[2]), "world")
+    else:
+        if(armPos[0] == handPos[0]):
+            p2.setTranslation((handPos[0], elbowPos[1], elbowPos[2]), "world")
+            pass
+        a = (armPos[0] - handPos[0]) / (armPos[1] - handPos[1])
+        b = armPos[0] - (armPos[1] * a)
+        c = (elbowPos[1]*a)+b
+        p2.setTranslation((c,elbowPos[1],elbowPos[2]), "world")
 
 class XMAutoRig(object):
     def __init__(self):
 
         self.window = "XMAutoRig"
         self.title = "XM Auto Rig"
-        self.size = (500,200)
+        self.size = (500,400)
 
         # close old window is open
         if pm.window(self.window, exists=True):
@@ -83,6 +133,7 @@ class XMAutoRig(object):
 
 
         self.rigLocator = {}
+        self.rigJoint= {}
         # create new window
         self.window = pm.window(self.window, title=self.title, widthHeight=self.size)
         currentRig = None
@@ -95,8 +146,14 @@ class XMAutoRig(object):
         pm.button(l="remove setup", c=lambda x: DeleteSetup())
         pm.setParent(u=True)
         self.settingFrame = pm.frameLayout(l="setting", en=False)
-        pm.button(l="fix elbow", c=lambda x: FixElbow())
+        pm.rowLayout(nc=2)
+        pm.button(l="fix elbow", c=lambda x: FixElbow("x",XMAutorigWindow.rigLocator["arm"], XMAutorigWindow.rigLocator["forearm"], XMAutorigWindow.rigLocator["hand"]))
+        pm.button(l="fix knee",c=lambda x: FixElbow("y", XMAutorigWindow.rigLocator["thigh"], XMAutorigWindow.rigLocator["leg"],XMAutorigWindow.rigLocator["foot"]))
+        pm.setParent(u=True)
         pm.intSliderGrp(l="spine joint", v=3, min=0,max=6, f=True)
+        pm.frameLayout(l="create rig")
+        pm.button(l="joint setup", c=lambda x: Createjoint())
+        pm.button(l="ctrl setup")
 
 
 
